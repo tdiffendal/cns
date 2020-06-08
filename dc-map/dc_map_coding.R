40856f6f2df86f9fd51b44e29aad760550490544
Theresa Diffendal <tdiff@umd.edu>
2020-06-08 13:28:01 -0400

    hmm

--- dc-map/dc_map_coding.R
+++ dc-map/dc_map_coding.R
@@ -7,6 +7,7 @@ library("xlsx")
 library(readr)
 library(tidyverse)
 library(dplyr)
+library(plyr)
 
 setwd("~/GitHub/cns/dc-map")
 
@@ -15,5 +16,11 @@ camps <- read.csv("geolocated_clearances.csv")
 geom <- read.csv("")
 
 gent$bins2 <- cut(gent$StrongExpansionDecline, breaks=c(-1400, -1000, -500, -1, 1, 500, 1000, 1900), labels=c("Extreme Gentrification", "Gentrification", "Little Gentrification", "No Gentrification", "Little Decline", "Decline", "Extreme Decline"))
+
+temp <- camps %>%
+  select(location) %>%
+  count()
+
+count(camps, "coordinates")
 
 write.xlsx(gent, "dc_gent_bins.xlsx")
